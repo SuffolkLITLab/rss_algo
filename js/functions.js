c3adc9da1fd6bf7fd1624b26fff4a26b31f73ab1
David Colarusso <colarusso@users.noreply.github.com>
2023-09-08 23:45:49 -0400

    reworked limiting given differing feed ordering

--- js/functions.js
+++ js/functions.js
@@ -572,8 +572,8 @@ document.addEventListener("DOMContentLoaded", function() {
                             }
                             
                             //console.log("Max items per feed:",(0.75*max_arts/rssFeeds.length))
-
-                            if ((pubDate >= backstop) && (pubDate >= lookback) && (j<Math.floor(1.45*max_arts/rssFeeds.length)) && (pubDate <= new Date())) {
+                            //if ((pubDate >= backstop) && (pubDate >= lookback) && (j<Math.floor(1.45*max_arts/rssFeeds.length)) && (pubDate <= new Date())) {
+                            if ((pubDate >= backstop) && (pubDate >= lookback) && (pubDate <= new Date())) {
                                 
                                 if (xml.documentElement.nodeName=="feed") {
                                     link = item.querySelector("link").attributes["href"].nodeValue;
