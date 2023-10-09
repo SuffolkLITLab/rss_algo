ef82012d80d2631e8b9c43ce1b09098061f47baa
David Colarusso <colarusso@users.noreply.github.com>
2023-10-09 12:47:31 -0400

    improved prox failure behavior

--- js/functions.js
+++ js/functions.js
@@ -26,6 +26,20 @@ function toggle_settings() {
 //    return articls_in_window
 //}
 
+async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 8000 } = options;
+    
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+  
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
+    });
+    clearTimeout(id);
+  
+    return response;
+  }
 
 const regex_always =  localStorage.getItem("regex_always") || "";
 document.getElementById("regex_always").value = regex_always;
@@ -295,16 +309,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
         var proxy_01 = "https://corsproxy.io/?"
         // set proxy_02 = "" if you only want to use proxy_01
-        var proxy_02 = "" //"https://tools.suffolklitlab.org/rss_proxy/?url="
+        var proxy_02 = "https://tools.suffolklitlab.org/rss_proxy/?url="
 
         feedUrl_prox = proxy_01+encodeURIComponent(feedUrl)       
-        const response = await fetch(feedUrl_prox);
+        const response = await fetchWithTimeout(feedUrl_prox, {timeout: 6000});
         if (!response.ok) {
             //throw new Error(`Request failed with status ${response.status}`);
             if (proxy_02!=""){
                 console.log("Trying corsproxy.io for "+feedUrl)
                 feedUrl_prox = proxy_02 + encodeURIComponent(feedUrl);
-                let response = await fetch(feedUrl_prox);    
+                let response = await fetchWithTimeout(feedUrl_prox, {timeout: 8000});    
             }
             if (!response.ok) {
                 console.log("Error fetching "+feedUrl)
