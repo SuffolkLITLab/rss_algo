2db805b4ac59a555e9c9630cc086e21a94094769
David Colarusso <colarusso@users.noreply.github.com>
2025-10-26 14:51:00 -0400

    added feed count to feed popup

--- js/functions.js
+++ js/functions.js
@@ -2694,7 +2694,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="feedListModalLabel">Feeds</h5>
+                            <h5 class="modal-title" id="feedListModalLabel">Feeds (${rssFeeds.length})</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
