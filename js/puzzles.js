c94b50388900ffb6db6ba0e0d72c0a5eb0d68cac
David Colarusso <colarusso@users.noreply.github.com>
2025-11-11 11:07:31 -0500

    puzzle edit

--- js/puzzles.js
+++ js/puzzles.js
@@ -1,4 +1,3 @@
 var puzzles = {
-    "2025-11-11" : `<iframe style="height: 90vh; width: 100%;" src="https://crosshare.org/embed/MJUbb4Pg4Unkt9re4zMP/305UqSnkS8eCfnettr5iC9sUtdj2" frameborder="0" allowfullscreen="true" allowtransparency="true" allow="clipboard-write *"></iframe>`,
-    "2025-11-17" : `<iframe style="height: 90vh; width: 100%;" src="https://crosshare.org/embed/MJUbb4Pg4Unkt9re4zMP/305UqSnkS8eCfnettr5iC9sUtdj2" frameborder="0" allowfullscreen="true" allowtransparency="true" allow="clipboard-write *"></iframe>`,
+    "2025-11-17" : `<iframe style="height: 90vh; width: 100%;" src="https://crosshare.org/embed/VPqGPMh0k9OGKwYiGa1O/305UqSnkS8eCfnettr5iC9sUtdj2" frameborder="0" allowfullscreen="true" allowtransparency="true" allow="clipboard-write *"></iframe>`,
 }
