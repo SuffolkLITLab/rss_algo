cc6f6c47f15bdd3f7a122695306ff3e3d70e8131
David Colarusso <colarusso@users.noreply.github.com>
2025-11-18 16:41:01 -0500

    remove placeholder puzzle

--- js/puzzles.js
+++ js/puzzles.js
@@ -1,4 +1,3 @@
 var puzzles = {
-    "2025-11-10" : `https://crosshare.org/embed/6vNcg3U64jpMPXVUYgbH/305UqSnkS8eCfnettr5iC9sUtdj2`,
     "2025-11-17" : `https://crosshare.org/embed/6vNcg3U64jpMPXVUYgbH/305UqSnkS8eCfnettr5iC9sUtdj2`,
 }
